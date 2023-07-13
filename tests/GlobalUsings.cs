c07cb8e1b3d531e8cc19c38908cc17cfd3034802
Callum Sykes <28632338+clicketyclackety@users.noreply.github.com>
2023-07-13 07:33:25 -0700

    Resolved setup

---
+++ tests/GlobalUsings.cs
@@ -0,0 +1,3 @@
+﻿global using NUnit.Framework;
+global using Rhino.Geometry;
+global using System;
--- tests/TestData/TestDataTests.cs
+++ tests/TestData/TestDataTests.cs
@@ -1,6 +1,6 @@
 ﻿using System.Collections;
 
-namespace Tests.TestData
+namespace Tests
 {
 
 	[RhinoFixture]
